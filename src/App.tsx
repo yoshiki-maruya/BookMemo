033083d465fca5c1c1a2fdb4d2e5c993ace9581d
yoshiki-maruya <yoshiki.maruya@gmail.com>
2022-12-20 00:34:55 +0900

    add: create sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
 import React from 'react'
-import './App.css'
+import Home from './components/pages/Home'
 
 function App() {
-  const [count, setCount] = React.useState(0)
-
   return (
-    <div className="text-center">
-      <h1 className='text-3xl'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Home />
   )
 }
 

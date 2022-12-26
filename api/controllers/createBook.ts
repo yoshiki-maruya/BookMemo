1f2319040086d6607730b4fa871b91eccc964f8f
yoshiki-maruya <yoshiki.maruya@gmail.com>
2022-12-26 19:58:30 +0900

    add: create controllers

---
+++ api/controllers/createBook.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from "@prisma/client";
+import express from "express";
+
+const prisma = new PrismaClient();
+
+type BookType = {
+  title: string,
+  content: string,
+}
+
+export const createBook = async (req: express.Request, res: express.Response) => {
+  const {title, content} = req.body;
+  try {
+    const bookInfo = await prisma.book.create({
+      data:{
+        title: title,
+        content: content,
+        }
+    });
+    res.status(201).json(bookInfo);
+    res.status
+  } catch (error: any) {
+    res.status(400).json({msg: error.message});
+  }
+}

51d5544391a63cc990839baf92893575b87fcc54
yoshiki-maruya <yoshiki.maruya@gmail.com>
2022-12-31 00:47:57 +0900

    update: create form

--- web/src/components/pages/Create.tsx
+++ web/src/components/pages/Create.tsx
@@ -20,26 +20,36 @@ const Create: React.FC = () => {
   }
 
   return (
-    <>
-      <h1>Create</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="title"
-          placeholder="title"
-          value={book?.title}
-          onChange={(e) => setBook({ title: e.target.value, content: book?.content })}
-        />
-        <input
-          type="text"
-          name="content"
-          placeholder="内容、気づき、感想"
-          value={book?.content}
-          onChange={(e) => setBook({ title: book?.title, content: e.target.value })}
-        />
-        <button>登録</button>
+    <div>
+      <h1 className="pt-16 text-center text-3xl">Create</h1>
+      <form className="mb-4 rounded bg-white px-52 pt-6 pb-8" onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label className="mb-2 block text-sm font-bold text-gray-700">Title</label>
+          <input
+            className="focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
+            type="text"
+            name="title"
+            placeholder="title"
+            value={book?.title}
+            onChange={(e) => setBook({ ...book, title: e.target.value })}
+          />
+        </div>
+        <div className="mb-6">
+          <label className="mb-2 block text-sm font-bold text-gray-700">Content</label>
+          <input
+            className="focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
+            type="text"
+            name="content"
+            placeholder="内容、気づき、感想"
+            value={book?.content}
+            onChange={(e) => setBook({ ...book, content: e.target.value })}
+          />
+        </div>
+        <div className="text-center">
+          <button className="focus:shadow-outline rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700 focus:outline-none">登録</button>
+        </div>
       </form>
-    </>
+    </div>
   )
 }
 

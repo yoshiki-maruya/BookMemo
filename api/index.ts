33d9c36e8026893b216fed85cb1a3ca0efc5fbfc
yoshiki-maruya <yoshiki.maruya@gmail.com>
2022-12-30 23:18:57 +0900

    add: cors setup

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import router from './routes/bookRoute';
+import cors from 'cors';
 
 const app: express.Express = express();
 const port = 8000;
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
